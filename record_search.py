e05d9399e41d8afa68d04e700cc771335f2cdd21
Falon Scheers <scheers@cs.ualberta.ca>
2015-03-21 23:49:27 -0600

    coming together now!

--- record_search.py
+++ record_search.py
@@ -1,16 +1,59 @@
+import datetime
+
 def DriverRecord(connection, curs):
 
    # select name, licence_no(drive_licence), addr, birthday, class(drive_licence), driving_condition    
 	# (driving_condition and restriction), expiring_date(drive_licence) 
 
     Name_DL = input("Enter driver name or licence number  ")
-
-    # decide if user entered a name or license number 
+    # decide if user entered a name or license number
+ 
     if Name_DL[0].isalpha(): # user entered name
-        pass
-
+        Name = Name_DL.upper()
+        query = "SELECT p.name as name, dl.licence_no as licence_no, p.addr as address, \
+                 p.birthday as DOB, dl.class as driving_class, \
+                 dl.expiring_date as expiring_date, dc.description as driving_condition \
+                 FROM people p, drive_licence dl, driving_condition dc, restriction r \
+                 WHERE TRIM(UPPER(p.name)) = :NA and \
+                       p.sin = dl.sin and \
+                       dc.c_id (+)= r.r_id and \
+                       r.licence_no (+)= dl.licence_no"
+        curs.execute(query, {'NA':Name})
+        
     else: # user entered licence number
-        pass
+        DL_num = int(Name_DL)
+        query = "SELECT p.name as name, dl.licence_no as licence_no, p.addr as address, \
+                 p.birthday as DOB, dl.class as driving_class, \
+                 dl.expiring_date as expiring_date, dc.description as driving_condition \
+                 FROM people p, drive_licence dl, driving_condition dc, restriction r \
+                 WHERE dl.licence_no = :DL and \
+                       p.sin = dl.sin and \
+                       dc.c_id (+)= r.r_id and \
+                       r.licence_no (+)= dl.licence_no"
+        curs.execute(query, {'DL':DL_num})
+
+    # get and format the info retrieved
+    Record = curs.fetchall()
+    if Record == []:
+        print("\n Record does not exist")
+        RecordSearch(connection, curs)
+
+    # print the results for all entires including duplicates
+    for row in Record:
+        name = Record[0][0]
+        licence_no = Record[0][1]
+        address = Record[0][2]
+        birthday = Record[0][3].date()
+        driving_class = Record[0][4]
+        expiring_date = Record[0][5].date()
+        driving_condition = Record[0][6]
+        print("\n Name: ", name)
+        print(" Address: ", address)
+        print(" Date of Birth: ", birthday)
+        print(" Driving Class: ", driving_class)
+        print(" Expiration date: ", expiring_date)
+        print(" Driving condition: ",driving_condition)
+        
         
 
 def DriverAbstract(connection, curs):
